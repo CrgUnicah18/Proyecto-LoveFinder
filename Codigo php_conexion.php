a53d23ac1eadb13bae4b58c4a8d0dea16f6221f4
CrgUnicah18 <108170493+CrgUnicah18@users.noreply.github.com>
2022-07-14 22:13:57 -0600

    Create Codigo php_conexion.php

---
+++ Codigo php_conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+    
+    /*
+    if($conexion){
+        echo 'Si conecto a la db';
+
+    }else{
+        echo 'No conecto a la db';
+    }
+    */
+
+
+?>
